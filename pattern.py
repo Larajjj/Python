ef347d12a1e5a05a659434e95c6df73e50f59f8d
Larajjj <42802765+Larajjj@users.noreply.github.com>
2020-07-23 15:55:54 +0530

    Create pattern.py

---
+++ pattern.py
@@ -0,0 +1,22 @@
+print("hey there!")
+
+# sequential
+x=1
+print(x)
+x=x+1
+print(x)
+
+# conditional
+x=5
+if x<10:
+    print("smaller")
+if x>20:
+    print('bigger')
+print('finish')
+
+# repeated
+n=5
+while n>0:
+    print(n)
+    n=n-1
+print('blastoff')
