b14c7d85f47110bed33608eb63b4431f1bfdd310
Larajjj <42802765+Larajjj@users.noreply.github.com>
2020-07-23 15:57:12 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -6,6 +6,26 @@ print(x)
 x=x+1
 print(x)
 
+x=5
+x=x+5
+print(x)
+y=x+10
+print(y)
+
+x=0.6
+print(x)
+x=3.9*x*(1-x)
+print(x)
+
+x=1098
+x= 1098%52
+print(x)
+
+x=2
+x=x**5
+print(x)
+
+
 # conditional
 x=5
 if x<10:
@@ -13,6 +33,7 @@ if x<10:
 if x>20:
     print('bigger')
 print('finish')
+
 
 # repeated
 n=5
